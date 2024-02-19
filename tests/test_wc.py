d9b07db29592539207481b4a0b4c00bd5ae7cdcf
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2024-02-20 00:21:22 +0200

    create ci.yml and refactored tests

--- tests/test_wc.py
+++ tests/test_wc.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 from app.cli.commands import wc
 
 LOG_FILE = "tests/resources/log.txt"
-BOOK_FILE = "tests/resources/book.txt"
+POEM_FILE = "tests/resources/poem.txt"
 NON_EXISTENT_FILE = "test/resources/bazz.yaml"
 TOTAL_SUFFIX = "total   "
 
@@ -12,69 +12,69 @@ def test_wc_single_file() -> None:
     runner = CliRunner()
     # no options
     assert runner.invoke(wc, [LOG_FILE]).output == f"       5       13       83 {LOG_FILE}\n"
-    assert runner.invoke(wc, [BOOK_FILE]).output == f"    7145    58164   342190 {BOOK_FILE}\n"
+    assert runner.invoke(wc, [POEM_FILE]).output == f"      13       77      414 {POEM_FILE}\n"
     # single option
     assert runner.invoke(wc, ["-c", LOG_FILE]).output == f"      83 {LOG_FILE}\n"
-    assert runner.invoke(wc, ["-c", BOOK_FILE]).output == f"  342190 {BOOK_FILE}\n"
+    assert runner.invoke(wc, ["-c", POEM_FILE]).output == f"     414 {POEM_FILE}\n"
 
     assert runner.invoke(wc, ["-l", LOG_FILE]).output == f"       5 {LOG_FILE}\n"
-    assert runner.invoke(wc, ["-l", BOOK_FILE]).output == f"    7145 {BOOK_FILE}\n"
+    assert runner.invoke(wc, ["-l", POEM_FILE]).output == f"      13 {POEM_FILE}\n"
 
     assert runner.invoke(wc, ["-w", LOG_FILE]).output == f"      13 {LOG_FILE}\n"
-    assert runner.invoke(wc, ["-w", BOOK_FILE]).output == f"   58164 {BOOK_FILE}\n"
+    assert runner.invoke(wc, ["-w", POEM_FILE]).output == f"      77 {POEM_FILE}\n"
 
     assert runner.invoke(wc, ["-m", LOG_FILE]).output == f"      83 {LOG_FILE}\n"
-    assert runner.invoke(wc, ["-m", BOOK_FILE]).output == f"  339292 {BOOK_FILE}\n"
+    assert runner.invoke(wc, ["-m", POEM_FILE]).output == f"     414 {POEM_FILE}\n"
 
     # combined options
     assert runner.invoke(wc, ["-cl", LOG_FILE]).output == f"       5       83 {LOG_FILE}\n"
-    assert runner.invoke(wc, ["-cl", BOOK_FILE]).output == f"    7145   342190 {BOOK_FILE}\n"
+    assert runner.invoke(wc, ["-cl", POEM_FILE]).output == f"      13      414 {POEM_FILE}\n"
 
     assert runner.invoke(wc, ["-wm", LOG_FILE]).output == f"      13       83 {LOG_FILE}\n"
-    assert runner.invoke(wc, ["-wm", BOOK_FILE]).output == f"   58164   339292 {BOOK_FILE}\n"
+    assert runner.invoke(wc, ["-wm", POEM_FILE]).output == f"      77      414 {POEM_FILE}\n"
 
     assert runner.invoke(wc, ["-cwl", LOG_FILE]).output == runner.invoke(wc, [LOG_FILE]).output
-    assert runner.invoke(wc, ["-cwl", BOOK_FILE]).output == runner.invoke(wc, [BOOK_FILE]).output
+    assert runner.invoke(wc, ["-cwl", POEM_FILE]).output == runner.invoke(wc, [POEM_FILE]).output
 
 
 def test_wc_file_list() -> None:
     runner = CliRunner()
     # no options
-    assert runner.invoke(wc, [LOG_FILE, BOOK_FILE]).output == (
+    assert runner.invoke(wc, [LOG_FILE, POEM_FILE]).output == (
         f"       5       13       83 {LOG_FILE}\n"
-        f"    7145    58164   342190 {BOOK_FILE}\n"
-        f"    7150    58177   342273 {TOTAL_SUFFIX}\n"
+        f"      13       77      414 {POEM_FILE}\n"
+        f"      18       90      497 {TOTAL_SUFFIX}\n"
     )
 
     # single option
-    assert runner.invoke(wc, ["-c", LOG_FILE, BOOK_FILE]).output == (
-        f"      83 {LOG_FILE}\n" f"  342190 {BOOK_FILE}\n" f"  342273 {TOTAL_SUFFIX}\n"
+    assert runner.invoke(wc, ["-c", LOG_FILE, POEM_FILE]).output == (
+        f"      83 {LOG_FILE}\n" f"     414 {POEM_FILE}\n" f"     497 {TOTAL_SUFFIX}\n"
     )
-    assert runner.invoke(wc, ["-l", LOG_FILE, BOOK_FILE]).output == (
-        f"       5 {LOG_FILE}\n" f"    7145 {BOOK_FILE}\n" f"    7150 {TOTAL_SUFFIX}\n"
+    assert runner.invoke(wc, ["-l", LOG_FILE, POEM_FILE]).output == (
+        f"       5 {LOG_FILE}\n" f"      13 {POEM_FILE}\n" f"      18 {TOTAL_SUFFIX}\n"
     )
-    assert runner.invoke(wc, ["-w", LOG_FILE, BOOK_FILE]).output == (
-        f"      13 {LOG_FILE}\n" f"   58164 {BOOK_FILE}\n" f"   58177 {TOTAL_SUFFIX}\n"
+    assert runner.invoke(wc, ["-w", LOG_FILE, POEM_FILE]).output == (
+        f"      13 {LOG_FILE}\n" f"      77 {POEM_FILE}\n" f"      90 {TOTAL_SUFFIX}\n"
     )
-    assert runner.invoke(wc, ["-m", LOG_FILE, BOOK_FILE]).output == (
-        f"      83 {LOG_FILE}\n" f"  339292 {BOOK_FILE}\n" f"  339375 {TOTAL_SUFFIX}\n"
+    assert runner.invoke(wc, ["-m", LOG_FILE, POEM_FILE]).output == (
+        f"      83 {LOG_FILE}\n" f"     414 {POEM_FILE}\n" f"     497 {TOTAL_SUFFIX}\n"
     )
 
     # combined options
-    assert runner.invoke(wc, ["-cl", LOG_FILE, BOOK_FILE]).output == (
+    assert runner.invoke(wc, ["-cl", LOG_FILE, POEM_FILE]).output == (
         f"       5       83 {LOG_FILE}\n"
-        f"    7145   342190 {BOOK_FILE}\n"
-        f"    7150   342273 {TOTAL_SUFFIX}\n"
+        f"      13      414 {POEM_FILE}\n"
+        f"      18      497 {TOTAL_SUFFIX}\n"
     )
-    assert runner.invoke(wc, ["-wm", LOG_FILE, BOOK_FILE]).output == (
+    assert runner.invoke(wc, ["-wm", LOG_FILE, POEM_FILE]).output == (
         f"      13       83 {LOG_FILE}\n"
-        f"   58164   339292 {BOOK_FILE}\n"
-        f"   58177   339375 {TOTAL_SUFFIX}\n"
+        f"      77      414 {POEM_FILE}\n"
+        f"      90      497 {TOTAL_SUFFIX}\n"
     )
 
     assert (
-        runner.invoke(wc, ["-cwl", LOG_FILE, BOOK_FILE]).output
-        == runner.invoke(wc, [LOG_FILE, BOOK_FILE]).output
+        runner.invoke(wc, ["-cwl", LOG_FILE, POEM_FILE]).output
+        == runner.invoke(wc, [LOG_FILE, POEM_FILE]).output
     )
 
     # non-existent file
