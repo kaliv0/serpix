2c35876a2e47f546ae1333ffddff1e99bd095818
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-04-15 16:08:19 +0300

    refactored cut tests

--- tests/test_cut.py
+++ tests/test_cut.py
@@ -5,10 +5,10 @@ from click.testing import CliRunner
 
 from app.cli.commands import cut
 
-TSV_FILE = "tests/resources/books1.tsv"
-CSV_FILE = "tests/resources/books2.csv"
-ALT_CSV_FILE = "tests/resources/books3.csv"
-NON_EXISTENT_FILE = "test/resources/bazz.yaml"
+TSV_FILE = "tests/resources/cut/books1.tsv"
+CSV_FILE = "tests/resources/cut/books2.csv"
+ALT_CSV_FILE = "tests/resources/cut/books3.csv"
+NON_EXISTENT_FILE = "test/resources/cut/bazz.yaml"
 
 runner = CliRunner()
 
@@ -47,32 +47,25 @@ def test_cut_fields_combined_options(cut_options) -> None:
         == subprocess.run(["cut", *cut_options, CSV_FILE], capture_output=True, text=True).stdout
     )
 
-    # error messages
-    ## no options
-    assert (
-        runner.invoke(cut, [TSV_FILE]).exception.args[0]
-        == "cut: you must specify a list of bytes, characters, or fields"
-    )
 
-    ## invalid options
-    assert (
-        runner.invoke(cut, ["-c1", "-b1,", TSV_FILE]).exception.args[0]
-        == "cut: only one type of list may be specified"
-    )
-    assert (
-        runner.invoke(cut, ["-c1", "-d,", TSV_FILE]).exception.args[0]
-        == "cut: an input delimiter may be specified only when operating on fields"
-    )
-    assert (
-        runner.invoke(cut, ["-f1", "-d_#_", TSV_FILE]).exception.args[0]
-        == "cut: the delimiter must be a single character"
-    )
-    assert (
-        runner.invoke(cut, ["-c1", "-s", TSV_FILE]).exception.args[0]
-        == "cut: suppressing non-delimited lines makes sense only when operating on fields"
-    )
+def test_cut_no_options_error() -> None:
+    assert runner.invoke(cut, [TSV_FILE]).exception.args[0] == subprocess.run(
+        ["cut", TSV_FILE], capture_output=True, text=True
+    ).stderr.replace("\nTry 'cut --help' for more information.\n", "")
 
-    ## invalid ranges
+
+@pytest.mark.parametrize(
+    "invalid_options", [("-c1", "-b1"), ("-c1", "-d"), ("-f1", "-d_#_"), ("-c1", "-s")]
+)
+def test_cut_invalid_options_error(invalid_options) -> None:
+    # NB: error messages lack final redirection to '--help' option
+    assert runner.invoke(cut, [*invalid_options, TSV_FILE]).exception.args[0] == subprocess.run(
+        ["cut", *invalid_options, TSV_FILE], capture_output=True, text=True
+    ).stderr.replace("\nTry 'cut --help' for more information.\n", "")
+
+
+def test_cut_invalid_ranges_error() -> None:
+    # NB: error messages differ from original
     assert (
         runner.invoke(cut, ["-c1-x", TSV_FILE]).exception.args[0]
         == "cut: invalid option value '1-x'"
@@ -82,95 +75,41 @@ def test_cut_fields_combined_options(cut_options) -> None:
         == "cut: invalid range with no endpoint: -"
     )
     assert runner.invoke(cut, ["-c-1-", TSV_FILE]).exception.args[0] == "cut: invalid range"
+
+
+@pytest.mark.parametrize("cut_option", ["-b1-3", "-c2-8"])
+def test_cut_file_list(cut_option) -> None:
     assert (
-        runner.invoke(cut, ["-f2", NON_EXISTENT_FILE]).exception.args[0]
-        == f"cut: {NON_EXISTENT_FILE}: No such file or directory"
+        runner.invoke(cut, [cut_option, TSV_FILE, CSV_FILE]).output
+        == subprocess.run(
+            ["cut", cut_option, TSV_FILE, CSV_FILE], capture_output=True, text=True
+        ).stdout
     )
 
 
-# def test_cut_file_list() -> None:
-#     # single option
-#     assert (
-#         runner.invoke(cut, ["-b1-3", TSV_FILE, CSV_FILE]).output
-#         == """pyt
-#     sna
-#     alp
-#     rob
-#     hor
-#     don
-#     ory
-#     pyt
-#     sna
-#     alp
-#     rob
-#     hor
-#     don
-#     orn
-#     Lor
-#     """
-#     )
-#     assert (
-#         runner.invoke(cut, ["-c2-8", TSV_FILE, CSV_FILE]).output
-#         == """ython\tP
-#     nail\tSS
-#     lpaca\tI
-#     obin\tMy
-#     orse\tLi
-#     onkey\tC
-#     ryx\tWri
-#     ython,P
-#     nail,SS
-#     lpaca,I
-#     obin,My
-#     orse,Li
-#     onkey,C
-#     rnyx,Wr
-#     orem Ip
-#     """
-#     )
+@pytest.mark.parametrize("cut_options", [("-f1", "-d,"), ("-f1", "-d,", "-s")])
+def test_cut_file_list_combined_options(cut_options) -> None:
+    assert (
+        runner.invoke(cut, [*cut_options, CSV_FILE, ALT_CSV_FILE]).output
+        == subprocess.run(
+            ["cut", *cut_options, CSV_FILE, ALT_CSV_FILE], capture_output=True, text=True
+        ).stdout
+    )
 
-#     # combined options
-#     assert (
-#         runner.invoke(cut, ["-f1", "-d,", CSV_FILE, ALT_CSV_FILE]).output
-#         == """python
-#     snail
-#     alpaca
-#     robin
-#     horse
-#     donkey
-#     ornyx
-#     Lorem Ipsum et cetera res vana
-#     English
-#     French
-#     Greek
-#     That book you never really read...
-#     Latin
-#     """
-#     )
-#     assert (
-#         runner.invoke(cut, ["-f1", "-d,", "-s", CSV_FILE, ALT_CSV_FILE]).output
-#         == """python
-#     snail
-#     alpaca
-#     robin
-#     horse
-#     donkey
-#     ornyx
-#     English
-#     French
-#     Greek
-#     Latin
-#     """
-#     )
-#     assert (
-#         runner.invoke(cut, ["-f2-3", NON_EXISTENT_FILE, TSV_FILE]).output
-#         == """cut: test/resources/bazz.yaml: No such file or directory
-#     Programming Python\t2010
-#     SSH, The Secure Shell\t2005
-#     Intermediate Perl\t2012
-#     MySQL High Availability\t2014
-#     Linux in a Nutshell\t2009
-#     Cisco IOS in a Nutshell\t2005
-#     Writing Word Macros\t1999
-#     """
-#     )
+
+def test_cut_nonexistent_file() -> None:
+    assert runner.invoke(cut, ["-f2", NON_EXISTENT_FILE]).exception.args[0] == subprocess.run(
+        ["cut", "-f2", NON_EXISTENT_FILE], capture_output=True, text=True
+    ).stderr.rstrip("\n")
+
+    # reads output from stdout and stderr
+    assert (
+        runner.invoke(cut, ["-f2-3", NON_EXISTENT_FILE, TSV_FILE]).output
+        == subprocess.Popen(
+            ["cut", "-f2-3", NON_EXISTENT_FILE, TSV_FILE],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        .communicate()[0]
+        .decode()
+    )
