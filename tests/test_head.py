e6b88b1d9ba76a8373b119c3b8c5e7f9d2e85832
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-04-19 13:14:15 +0300

    refactored head tests

--- tests/test_head.py
+++ tests/test_head.py
@@ -1,3 +1,6 @@
+import subprocess
+
+import pytest
 from click.testing import CliRunner
 
 from app.cli.commands import head
@@ -7,66 +10,33 @@ POEM_FILE = "tests/resources/poem.txt"
 NON_EXISTENT_FILE = "test/resources/bazz.yaml"
 DIR_PATH = "test/resources/"
 
+runner = CliRunner()
 
-def test_head_single_file() -> None:
-    runner = CliRunner()
-    # no options
-    expected_no_opts_1 = """Hello world
-Ciao ragazzi
-Bon jour
-Bratwurst
-De lana caprina rixatur
-Aien aristeuein
-"""
 
-    expected_no_opts_2 = """Good-night? ah! no; the hour is ill
-Which severs those it should unite;
-Let us remain together still,
-Then it will be GOOD night.
+@pytest.mark.parametrize("file", [LOG_FILE, POEM_FILE])
+def test_head_no_options(file: str) -> None:
+    assert runner.invoke(head, [file]).output.rstrip("\n") == subprocess.run(
+        ["head", file], capture_output=True, text=True
+    ).stdout.rstrip("\n")
 
-How can I call the lone night good,
-Though thy sweet wishes wing its flight?
-Be it not said, thought, understood -
-Then it will be - GOOD night.
 
-"""
-    assert runner.invoke(head, [LOG_FILE]).output == expected_no_opts_1
-    assert runner.invoke(head, [POEM_FILE]).output == expected_no_opts_2
+@pytest.mark.parametrize(
+    "head_options, file",
+    [("-n 3", LOG_FILE), ("-n 3", POEM_FILE), ("-c 40", LOG_FILE), ("-q", LOG_FILE)],
+)
+def test_head(head_options: str, file: str) -> None:
+    assert runner.invoke(head, [head_options, file]).output.rstrip("\n") == subprocess.run(
+        ["head", head_options, file], capture_output=True, text=True
+    ).stdout.rstrip("\n")
 
-    # single option
-    expected_v_n3 = """==> tests/resources/log.txt <==
-Hello world
-Ciao ragazzi
-Bon jour
-"""
 
-    expected_v_minus_n3 = """==> tests/resources/poem.txt <==
-Good-night? ah! no; the hour is ill
-Which severs those it should unite;
-Let us remain together still,
-Then it will be GOOD night.
-
-How can I call the lone night good,
-Though thy sweet wishes wing its flight?
-Be it not said, thought, understood -
-Then it will be - GOOD night.
-
-To hearts which near each other move
-"""
-    assert runner.invoke(head, ["-v", "-n 3", LOG_FILE]).output == expected_v_n3
-    assert runner.invoke(head, ["-v", "-n -3", POEM_FILE]).output == expected_v_minus_n3
-
-    expected_c4 = "Hello world\nCiao ragazzi\nBon jour\nBratwu\n"
-    assert runner.invoke(head, ["-c 40", LOG_FILE]).output == expected_c4
-
-    # error messages
+def test_head_error_message() -> None:
     expected_value_error = "Contradicting flags passed: 'quiet' and 'verbose'"
-    assert runner.invoke(head, ["-q", "-v", LOG_FILE]).exception.args[0] == expected_value_error
+    assert runner.invoke(head, ["-qv", LOG_FILE]).exception.args[0] == expected_value_error
 
 
-def test_wc_file_list() -> None:
-    runner = CliRunner()
-    # no options
+# NB: serpix head puts additional empty lines between different files in the output for better readabilty
+def test_wc_file_list_no_options() -> None:
     expected_multiple_no_opts = """==> tests/resources/log.txt <==
 Hello world
 Ciao ragazzi
@@ -89,7 +59,8 @@ Then it will be - GOOD night.
 """
     assert runner.invoke(head, [LOG_FILE, POEM_FILE]).output == expected_multiple_no_opts
 
-    # single option
+
+def test_wc_file_list() -> None:
     expected_multiple_n5 = """==> tests/resources/log.txt <==
 Hello world
 Ciao ragazzi
@@ -106,7 +77,8 @@ Then it will be GOOD night.
 """
     assert runner.invoke(head, ["-n 5", LOG_FILE, POEM_FILE]).output == expected_multiple_n5
 
-    # combined options
+
+def test_wc_file_list_combined_options() -> None:
     expected_multiple_q_n6 = """Hello world
 Ciao ragazzi
 Bon jour
@@ -123,7 +95,8 @@ How can I call the lone night good,
 """
     assert runner.invoke(head, ["-q", "-n 6", LOG_FILE, POEM_FILE]).output == expected_multiple_q_n6
 
-    # non-existent file
+
+def test_wc_file_list_non_existent_file() -> None:
     expected_multiple_non_existent = f"""head: cannot open '{NON_EXISTENT_FILE}' for reading: No such file or directory
 
 Good-night? ah! no; the hour is ill
@@ -138,7 +111,8 @@ How can I call the lone night good,
         == expected_multiple_non_existent
     )
 
-    # file-path is directory
+
+def test_wc_file_list_invalid_file_path() -> None:
     assert (
         runner.invoke(head, [DIR_PATH, NON_EXISTENT_FILE, POEM_FILE]).output
         == f"""head: cannot open '{DIR_PATH}' for reading: No such file or directory
